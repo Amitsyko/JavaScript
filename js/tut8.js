3b5c78033a67a4f838c60674315b295d6b4b767c
Amit <vishamit8827@gmail.com>
2023-12-21 16:41:48 +0530

    Old Js

---
+++ js/tut8.js
@@ -0,0 +1,59 @@
+console.log("Welcome to tut8.js ---- we are learning in this toutrial if else condition");
+
+/*-------------------------------*/
+
+const age = 65;
+
+if (age != 18) {
+    console.log("Age is 18")
+}
+else if (age == 65) {
+    console.log("Age is 65")
+}
+else {
+    console.log('Age is not defined !!')
+}
+
+
+
+/*-----Check Variable is defined or not-----*/
+
+// const vari = 30;
+
+if (typeof vari !== "undefined") {
+    console.log("vari is defined")
+} else {
+    console.log('vari is not defined')
+}
+
+/*---Check both condition are true or not---*/
+
+const person = 19;
+const canDrive = false;
+
+if (canDrive || person >= 19) {
+    console.log('You can Drvie');
+} else {
+    console.log('You cannot Drvie');
+}
+
+/*---Switch Case Condition---*/
+
+const marks = 80;
+
+switch (marks) {
+    case 20:
+        console.log("your marks is 20");
+        break;
+
+    case 40:
+        console.log("your marks is 40");
+        break;
+
+    case 60:
+        console.log("your marks is 60");
+        break;
+
+    default:
+        console.log("Your marks is not exit here");
+}
