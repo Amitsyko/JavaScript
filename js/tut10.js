a98d7dcff836441aa8957ef769a6dfc6e5cc3c3b
Amit <vishamit8827@gmail.com>
2023-12-26 15:35:51 +0530

    old js

---
+++ js/tut10.js
@@ -0,0 +1,46 @@
+console.log("Welcome to tut10.js ---- We are Learning the fucntions in JavaScript");
+
+// const mygreet = function greet(name,mood="Thanks a lot..!"){
+//     let msg = `Hey Happy BirthDay ${name} How's the day ${mood}`;
+//     return msg;
+// }
+
+// let name ="Amit";
+// let mood = "Good";
+
+// let val = mygreet(name, "I am very Happy");
+// console.log(val);
+// // greet(mood);
+
+// const myObj = {
+//     Vname : "Anuj",
+//     game : function(){
+//         return "GTA";
+//     }
+// }
+// console.log(myObj.game());
+
+
+// arr=["fruit","vegitables","furniture"];
+
+// arr.forEach(function(element,index,array) {
+//     console.log(element,index,array);
+// });
+
+
+if (1) {
+    var i = 234;
+    console.log(i);
+}
+console.log(i);
+
+
+function ui(name) {
+    let i = 9;
+    console.log(i);
+    return `This is a ${name} ui`;
+}
+console.log(ui("Amit"), i);
+
+
+
---
+++ js/tut9.js
@@ -0,0 +1,24 @@
+console.log('Welcome to the tut9.js ---- We learn in this toturail loops');
+
+// for(let i=0;i<100;i++)
+// {
+//     console.log(i);
+// }   
+
+// let = j=0;
+// while(j<100){
+//     j++;
+//     console.log(j)
+// }
+
+
+let = k=0;
+do{
+    if(k===5){
+        k++;
+        continue;
+    }
+    k++;
+    console.log(k);
+}while(k<10)
+console.log("Done-!! Happy Ending");
