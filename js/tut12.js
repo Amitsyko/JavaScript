de9b9ceff5b08a6fab0f5baa82c1c9d1b56346c6
Amit <vishamit8827@gmail.com>
2024-01-04 17:54:10 +0530

    Old Js

---
+++ js/tut12.js
@@ -0,0 +1,24 @@
+console.log("Hey Welcome to tut12.js");
+
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms[0];
+// a = document.links;
+// a = document.links[0];
+// a = document.forms[a];
+
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// });
+// console.log(a);
+
+let c = document.images;
+let d = document.scripts;
+
+Array.from(c).forEach(function(element){
+    console.log(element);
+});
+Array.from(d).forEach(function(element){
+    console.log(element);
+});
