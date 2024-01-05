b660979a65ce9a293cbf924fe7f0aea5b11eb6f8
Amit <vishamit8827@gmail.com>
2024-01-05 18:53:48 +0530

    Old Js

---
+++ js/tut14.js
@@ -0,0 +1,63 @@
+console.log("Hey Welcome to tut14.js---We can learn in this tut14.js element selectors");
+
+/*----Element Selectors ----
+
+1. Single Element Selector
+2. Multiple Element Selector
+
+*/
+
+
+// ----Single Element Selector----
+
+let element = document.getElementById("myfirst");
+// element = element.className;
+// element = element.childNodes;
+// element = element.parentNode;
+element.style.color = "red";
+// element.innerText = "Doremon";
+
+
+// element.innerText = "Doremon";
+// console.log(element);
+
+// let photo = document.getElementsByTagName("img");
+// photo.innerHTML=<img src='https://debtdistressrescue.com.au/wp-content/uploads/2023/12/Group-1957.png'/>
+// console.log(photo);
+
+
+
+
+
+// let sel = document.querySelector("#myfirst");
+
+
+let sel = document.querySelector(".child");
+sel.style.color="red";
+sel.innerText= "Doremon";
+//   console.log(sel)
+
+
+
+
+// ------Multi Element Selectors------
+
+let elems = document.getElementsByClassName("child");
+// elems.innerText = "Amit";
+// elems = document.getElementsByClassName("container");
+// elems = document.getElementsByTagName("div");
+// console.log(elems);
+
+// Array.from(elems).forEach(element => {
+//     element.innerText = "red";
+//    console.log(element);
+// });
+
+
+
+for (let index = 0; index < elems.length; index++) {
+    const element = elems[index];
+    element.style.color = "#ff25d7";
+    console.log(element)
+}
+
---
+++ js/tut15.js
@@ -0,0 +1,6 @@
+console.log("Hey Welcome to tut15.js----In this");
+
+let amit  = 'Amit';
+let amit1 = 'Hey Welcome to tut15.js----In this';
+
+console.log(amit,amit1);
