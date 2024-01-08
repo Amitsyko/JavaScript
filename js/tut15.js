bc2dc07196ff1d4b6a35f198472d035298eef4ec
Amit <vishamit8827@gmail.com>
2024-01-08 19:04:07 +0530

    Old Js

--- js/tut15.js
+++ js/tut15.js
@@ -1,6 +1,40 @@
-console.log("Hey Welcome to tut15.js----In this");
+console.log("Hey Welcome to tut15.js-----We can learn in this tut ----Transversing Parent and childerns");
 
-let amit  = 'Amit';
-let amit1 = 'Hey Welcome to tut15.js----In this';
+let cont = document.querySelector(".no");
+cont = document.querySelector(".container");
 
-console.log(amit,amit1);
+let nodeName = cont.childNodes[5].nodeName;
+let nodeType = cont.childNodes[10].nodeType;
+
+// console.log(nodeName);
+console.log(nodeType);
+
+/*
+
+NodeType list
+
+1- Eleement
+2- Attribute
+3- Text Node
+8- Comment
+9- Document
+10- Doctype
+
+*/
+
+// console.log(cont.childNodes);
+// console.log(cont.children);
+
+let container = document.querySelector('div.container');
+
+// console.log(container.children[1].children[0].children);
+
+// console.log(container.firstElementChild);
+// console.log(container.lastElementChild);
+
+
+// console.log(container.children);
+// console.log(container.childElementCount);
+
+console.log(container.parentNode);
+console.log(container.nextElementSibling.nextElementSibling);
